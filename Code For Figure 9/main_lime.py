063ca530425f2bffc7656e373beda5fd5245e398
FengleiFan <49825909+FengleiFan@users.noreply.github.com>
2020-10-03 16:22:20 -0400

    Implement LIME
    Implement LIME

---
+++ Code For Figure 9/main_lime.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import sklearn
+import sklearn.datasets
+import sklearn.ensemble
+import numpy as np
+import lime
+import lime.lime_tabular
+
+### Before running this code, you need to install 
+
+np.random.seed(1)
+
+iris = sklearn.datasets.load_iris()
+
+train, test, labels_train, labels_test = sklearn.model_selection.train_test_split(iris.data, iris.target, train_size=0.80)
+
+rf = sklearn.ensemble.RandomForestClassifier(n_estimators=500)
+rf.fit(train, labels_train)
+
+sklearn.metrics.accuracy_score(labels_test, rf.predict(test))
+
+explainer = lime.lime_tabular.LimeTabularExplainer(train, feature_names=iris.feature_names, class_names=iris.target_names, discretize_continuous=True)
+
+i = np.random.randint(0, test.shape[0])
+exp = explainer.explain_instance(test[i], rf.predict_proba, num_features=4, top_labels=3)
+
+exp.as_pyplot_figure()
+
